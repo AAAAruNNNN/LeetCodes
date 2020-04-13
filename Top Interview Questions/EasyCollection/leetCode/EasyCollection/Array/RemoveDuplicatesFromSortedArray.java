f78f7e94cbdd223e5f6f5bc4a7e63abea20926be
Kumar K,Arun <Arun.KumarK@cerner.com>
2020-04-13 23:16:31 +0530

    First commit with Project structure and soln to few easy questions

---
+++ Top Interview Questions/EasyCollection/leetCode/EasyCollection/Array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,27 @@
+package leetCode.EasyCollection.Array;
+
+public class RemoveDuplicatesFromSortedArray {
+	public static void main(String args[]) {
+		/*
+		 * This function returns only a int value (length which is needed to print the
+		 * array, which is taken care by leetcode
+		 */
+		System.out.println(removeDuplicates(new int[] { 1, 1, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6 }));
+	}
+
+	public static int removeDuplicates(int[] nums) {
+		int count, pos = 1;
+		if (nums.length == 0)
+			count = 0;
+		else
+			count = 1;
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] != nums[i + 1]) {
+				nums[pos++] = nums[i + 1];
+				count++;
+			}
+		}
+		return count;
+	}
+
+}
---
+++ Top Interview Questions/EasyCollection/leetCode/EasyCollection/Array/SingleNumber.java
@@ -0,0 +1,34 @@
+package leetCode.EasyCollection.Array;
+
+//import java.util.Arrays;
+
+public class SingleNumber {
+	public static int singleNumber(int[] nums) {
+		/*
+		 * 
+		 * Arrays.sort(nums);
+		 * if(nums.length < 2)
+		 * 	return nums[0];
+		 * for(int i = 0; i < nums.length - 1 ; i++)
+		 * {
+		 * 	if(nums[i]!=nums[++i])
+		 * 		return nums[i-1];
+		 * 	if(i == nums.length - 1)
+		 * 		return nums[i];
+		 * }
+		 * return 0;
+		 
+		 */
+
+		// better solution:
+		int a = 0;
+		for (int elem : nums) {
+			a ^= elem;
+		}
+		return a;
+	}
+
+	public static void main(String args[]) {
+		System.out.println(singleNumber(new int[] { 2, 2, 3, 3, 5, 8, 8 }));
+	}
+}
