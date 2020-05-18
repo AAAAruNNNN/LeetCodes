ddfcb7a29dd99370349016d0c2f5a55318305c86
Arun <aruncrica@gmail.com>
2020-05-18 23:56:10 +0530

    Started merge two sortled list

---
+++ Top Interview Questions/EasyCollection/LinkedLists/MergeTwoSortedList.java
@@ -0,0 +1,12 @@
+package LinkedLists;
+
+public class MergeTwoSortedList {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode l3 = l1;
+        while(l1 != null && l2 != null){
+            if(l1.val > l2.val){
+                
+            }
+        }
+    }
+}
