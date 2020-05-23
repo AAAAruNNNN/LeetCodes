53a994bcc1aa5d613ba653ac9696e2dda0552c92
Arun <aruncrica@gmail.com>
2020-05-23 20:32:58 +0530

    Reverse Integer made better

--- Top Interview Questions/EasyCollection/Strings/ReverseInteger.java
+++ Top Interview Questions/EasyCollection/Strings/ReverseInteger.java
@@ -5,18 +5,26 @@ public static void main(String args[]) {
 	System.out.println(reverseInteger(-1234556));
 }
 public static int reverseInteger(int n) {
-	int retVal = 0;
-	int pop;
-	while(n!=0) {
-		pop = n%10;
-		n /= 10;
-		if(retVal > Integer.MAX_VALUE/10 || retVal == Integer.MAX_VALUE/10 && pop > 7) 
-			return 0;
-		if(retVal < Integer.MIN_VALUE/10 || retVal == Integer.MAX_VALUE/10 && pop < -8) 
-			return 0;
-		retVal = retVal*10 + pop;
-	}
+//	int retVal = 0;
+//	int pop;
+//	while(n!=0) {
+//		pop = n%10;
+//		n /= 10;
+//		if(retVal > Integer.MAX_VALUE/10 || retVal == Integer.MAX_VALUE/10 && pop > 7) 
+//			return 0;
+//		if(retVal < Integer.MIN_VALUE/10 || retVal == Integer.MAX_VALUE/10 && pop < -8) 
+//			return 0;
+//		retVal = retVal*10 + pop;
+//	}
+//	
+//	return retVal;
 	
-	return retVal;
+    long ret=0, pop = 0;
+    while(n!=0){
+        pop = n%10;
+        n/=10;
+        ret = ret*10 + pop;
+    }
+    return ret>Integer.MAX_VALUE || ret<Integer.MIN_VALUE?0:(int)ret;
 }
 }
