9b95f1cb18d96871f10d3173373f7bbcde9bbaa2
Arun <aruncrica@gmail.com>
2020-06-24 14:20:14 +0530

    PalindromeLinkedList recursive soln

--- Top Interview Questions/EasyCollection/LinkedLists/PalindromeLinkedList.java
+++ Top Interview Questions/EasyCollection/LinkedLists/PalindromeLinkedList.java
@@ -1,56 +1,66 @@
 package LinkedLists;
+
 import java.util.ArrayList;
 
 public class PalindromeLinkedList {
-	
+
 	public static void main(String args[]) {
 		SinglyLinkedList head = new SinglyLinkedList(132);
 		head.next = new SinglyLinkedList(132);
-		System.out.println(isPalindrome(head));
+		System.out.println(isPalindromeRec(head));
 	}
+
+	public static boolean isPalindrome(SinglyLinkedList head) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		while (head != null) {
+			arr.add(head.val);
+			head = head.next;
+		}
+		for (int i = 0, j = arr.size() - 1; i < j; i++, j--) {
+			int x = arr.get(i);
+			int y = arr.get(j);
+			if (x != y)
+				return false;
+		}
+		return true;
+	}
+
+	public static boolean isPalindromeRec(SinglyLinkedList head) {
+		SinglyLinkedList dummy = new SinglyLinkedList(0, head);
+		SinglyLinkedList slow = dummy;
+		SinglyLinkedList fast = dummy;
+		SinglyLinkedList revHalfList;
+
+		while (fast != null) {
+			if (fast.next == null)
+				break;
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		if (fast == null)
+			revHalfList = reverseList(slow);
+		else
+			revHalfList = reverseList(slow.next);
+
+		while (head != null && revHalfList != null) {
+			if (head.val != revHalfList.val)
+				return false;
+			head = head.next;
+			revHalfList = revHalfList.next;
+		}
+		return true;
+	}
+
 	
-    public static boolean isPalindrome(SinglyLinkedList head) {
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        while(head!=null){
-            arr.add(head.val);
-            head = head.next;
-        }
-        for(int i = 0, j = arr.size()-1; i<j;i++,j--){
-            int x = arr.get(i);
-            int y = arr.get(j);
-            if(x != y)
-                return false;
-        }
-        return true;
-    }
-//    public boolean isPalindrome(SinglyLinkedList head) {
-//        if(head == null || head.next == null)
-//            return true;
-//        if(head.next.next == null){
-//            if(head.val == head.next.val)
-//                return true;
-//            return false;
-//        }
-//        Stack<Integer> st = new Stack<Integer>();
-//        SinglyLinkedList slow = head;
-//        SinglyLinkedList fast = head;
-//        while(fast != null && fast.next!= null){
-//            st.push(slow.val);
-//            slow = slow.next;
-//            fast = fast.next.next;
-//        }
-//        if(fast != null)
-//            slow = slow.next;
-//     
-//        
-//        
-//        while(slow!= null && !st.isEmpty()){
-//            
-//            if(slow.val == st.pop().intValue())
-//                slow = slow.next;
-//            else 
-//                return false;
-//        }
-//        return true;
-//    }
+
+	public static SinglyLinkedList reverseList(SinglyLinkedList head) {
+		if (head == null || head.next == null)
+			return head;
+		SinglyLinkedList p = reverseList(head.next);
+		SinglyLinkedList nextNode = head.next;
+		head.next = null;
+		nextNode.next = head;
+		return p;
+	}
+
 }
