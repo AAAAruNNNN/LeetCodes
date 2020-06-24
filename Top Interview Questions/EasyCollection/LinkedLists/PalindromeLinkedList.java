72754d9d08166eb1bda7d7e81b4c5d5703cd2e7f
Arun <aruncrica@gmail.com>
2020-06-24 17:20:52 +0530

    Complete recursive soln for PalindromeLinkedList

--- Top Interview Questions/EasyCollection/LinkedLists/PalindromeLinkedList.java
+++ Top Interview Questions/EasyCollection/LinkedLists/PalindromeLinkedList.java
@@ -3,31 +3,37 @@ package LinkedLists;
 import java.util.ArrayList;
 
 public class PalindromeLinkedList {
+	SinglyLinkedList left;
 
 	public static void main(String args[]) {
-		SinglyLinkedList head = new SinglyLinkedList(132);
+		SinglyLinkedList head = new SinglyLinkedList(12);
 		head.next = new SinglyLinkedList(132);
-		System.out.println(isPalindrome(head));
+		PalindromeLinkedList obj = new PalindromeLinkedList();
+		System.out.println(obj.isPalindromeCompleteRecursive(head));
 	}
 
-	public static boolean isPalindrome(SinglyLinkedList head) {
+	public boolean isPalindrome(SinglyLinkedList head) {
 		ArrayList<Integer> arr = new ArrayList<Integer>();
 		while (head != null) {
 			arr.add(head.val);
 			head = head.next;
 		}
-		/* Integer has its int values boxed so if we compare two Integer directly it checks if both the values are refering 
-		 * to the same object rather than checking if both has same value. if int and Integer values are compared then Integer
-		 * value automatically casts to unboxed primitive int. To check if two Integer (boxed) values use .equals() method or 
-		 * get the intValue() and compare them. */
+		/*
+		 * Integer has its int values boxed so if we compare two Integer directly it
+		 * checks if both the values are refering to the same object rather than
+		 * checking if both has same value. if int and Integer values are compared then
+		 * Integer value automatically casts to unboxed primitive int. To check if two
+		 * Integer (boxed) values use .equals() method or get the intValue() and compare
+		 * them.
+		 */
 		for (int i = 0, j = arr.size() - 1; i < j; i++, j--) {
-			if (arr.get(i).intValue() != arr.get(j).intValue())         
+			if (arr.get(i).intValue() != arr.get(j).intValue())
 				return false;
 		}
 		return true;
 	}
 
-	public static boolean isPalindromeRec(SinglyLinkedList head) {
+	public boolean isPalindromePartialRecursive(SinglyLinkedList head) {
 		SinglyLinkedList dummy = new SinglyLinkedList(0, head);
 		SinglyLinkedList slow = dummy;
 		SinglyLinkedList fast = dummy;
@@ -53,9 +59,7 @@ public class PalindromeLinkedList {
 		return true;
 	}
 
-	
-
-	public static SinglyLinkedList reverseList(SinglyLinkedList head) {
+	public SinglyLinkedList reverseList(SinglyLinkedList head) {
 		if (head == null || head.next == null)
 			return head;
 		SinglyLinkedList p = reverseList(head.next);
@@ -63,6 +67,27 @@ public class PalindromeLinkedList {
 		head.next = null;
 		nextNode.next = head;
 		return p;
+	}
+
+	public boolean isPalindromeCompleteRecursive(SinglyLinkedList head) {
+
+		left = head;
+		return palindromeCheckRecursive(head);
+	}
+
+	public boolean palindromeCheckRecursive(SinglyLinkedList head) {
+		if (head == null)
+			return true;
+		boolean check = palindromeCheckRecursive(head.next);
+		if (check == false)
+			return false;
+		if (left.val == head.val) {
+			left = left.next;
+			return true;
+		} else
+			check = false;
+		return check;
+
 	}
 
 }
