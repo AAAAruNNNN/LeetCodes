0d33dc15cb695210c9de4d0ace79ad4db0f92b2e
Arun <aruncrica@gmail.com>
2020-06-29 13:26:56 +0530

    Binary Tree Level order proper impl

--- Top Interview Questions/EasyCollection/Trees/BinaryTreeLevelOrderTraversal.java
+++ Top Interview Questions/EasyCollection/Trees/BinaryTreeLevelOrderTraversal.java
@@ -1,6 +1,9 @@
 package Trees;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class BinaryTreeLevelOrderTraversal {
@@ -14,26 +17,29 @@ public class BinaryTreeLevelOrderTraversal {
 		  // 9  20
 		  //   /  \
 		  //  15   7
-		obj.levelOrder(root);
+		System.out.println(Arrays.toString((obj.levelOrder(root).toArray())));
 	}
 	
-    public void levelOrder(TreeNode root) {
-        //List<List<Integer>> list = new ArrayList<List<Integer>>();
-        if(root == null) {
-        	System.out.println("null");
-            return ;        	
-        }
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        q.add(root);
-        while(!q.isEmpty()){
-            //List<Integer> rowList = new ArrayList<Integer>();
-            TreeNode node = q.remove();
-            System.out.print(node.val + " " );
-            if(node.left !=null) q.add(node.left);
-            if(node.right != null) q.add(node.right);
-//            rowList.add(node.val);
+	public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        List<Integer> rowList = new ArrayList<Integer>();
+        if(root == null)
+            return list;
+        Queue<TreeNode> qone = new LinkedList<TreeNode>(), qtwo = new LinkedList<TreeNode>();
+        qone.add(root);
+        while(!qone.isEmpty()){
+            TreeNode node = qone.remove();
+            rowList.add(node.val);
+            if(node.left !=null) qtwo.add(node.left);
+            if(node.right != null) qtwo.add(node.right);
+            if(qone.isEmpty()){
+                list.add(rowList);
+                rowList = new ArrayList<Integer>();
+                qone = qtwo;
+                qtwo = new LinkedList<TreeNode>();
+            }
             
         }
-        return;
+        return list;
     }
 }
